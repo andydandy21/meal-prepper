68a1c256e9b8d72453b00e6d9b61457263150680
andydandy21 <a.smithb21@gmail.com>
2023-12-16 12:28:47 -0600

    init LambdaStack

--- bin/meal-prepper.ts
+++ bin/meal-prepper.ts
@@ -3,9 +3,13 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { DataStack } from "../lib/DataStack";
 import { InitDataResourceStack } from "../lib/InitDataResourceStack";
+import { LambdaStack } from "../lib/LambdaStack";
 
 const app = new cdk.App();
 const dataStack = new DataStack(app, "MPDataStack");
 new InitDataResourceStack(app, "MPResourceStack", {
+  mealPrepperTable: dataStack.mealPrepperTable,
+});
+const lambdaStack = new LambdaStack(app, "MPLambdaStack", {
   mealPrepperTable: dataStack.mealPrepperTable,
 });
---
+++ lib/LambdaStack.ts
@@ -0,0 +1,42 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import { ITable } from "aws-cdk-lib/aws-dynamodb";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+
+interface LambdaStackProps extends StackProps {
+  mealPrepperTable: ITable;
+}
+
+export class LambdaStack extends Stack {
+  public readonly ingredientLambda: LambdaIntegration;
+
+  constructor(scope: Construct, id: string, props: LambdaStackProps) {
+    super(scope, id, props);
+
+    const ingredientLambda = new NodejsFunction(this, "MPIngredientLambda", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "handler",
+      entry: join(__dirname, "..", "lambda", "ingredient", "handler.ts"),
+      environment: {
+        TABLE_NAME: props.mealPrepperTable.tableName,
+      },
+    });
+    ingredientLambda.addToRolePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        resources: [props.mealPrepperTable.tableArn],
+        actions: [
+          "dynamodb:Query",
+          "dynamodb:GetItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:UpdateItem",
+        ],
+      }),
+    );
+    this.ingredientLambda = new LambdaIntegration(ingredientLambda);
+  }
+}
